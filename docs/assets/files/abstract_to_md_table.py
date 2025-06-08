ea99fc690748f278c76ddb6bda580b03d3202a20
Nabil Freij <nabil.freij@gmail.com>
2025-06-08 10:10:20 -0700

    Updated stuff

--- docs/assets/files/abstract_to_md_table.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-csv_file = "./accepted_abstracts.csv"
-df = pd.read_csv(csv_file)
-markdown_table = df.to_markdown(index=False)
-with open("abstracts_table.md", "w") as f:
-    f.write(markdown_table)
